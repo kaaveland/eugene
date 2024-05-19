f0240d7bd9f8e438ab5f4d2825a55f04a26df8c7
Robin Kåveland <kaaveland@gmail.com>
2024-05-19 22:05:27 +0200

    Add the generated toc onto SUMMARY.md in github workflow

--- build_doc.sh
+++ build_doc.sh
@@ -4,4 +4,4 @@ set -e -u -f -o pipefail
 
 sed -i '' -e 's/[[:space:]]*$//' docs/src/SUMMARY.md
 cat docs/src/generated_hint_toc.md >> docs/src/SUMMARY.md
-cd docs && mdbook build && git checkout -- docs/src/SUMMARY.md
+mdbook build docs && git checkout -- docs/src/SUMMARY.md

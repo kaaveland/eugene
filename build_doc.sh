7ffd3380ac550d54c26471592299531e11441fbf
Robin Kåveland <kaaveland@gmail.com>
2024-05-19 22:16:53 +0200

    Build docs with ./mdbook if present

--- build_doc.sh
+++ build_doc.sh
@@ -4,4 +4,11 @@ set -e -u -f -o pipefail
 
 sed -i '' -e '${/^$/d;}' docs/src/SUMMARY.md || true
 cat docs/src/generated_hint_toc.md >> docs/src/SUMMARY.md
-mdbook build docs && git checkout -- docs/src/SUMMARY.md
+
+if [[ -x "./mdbook" ]]; then
+  ./mdbook build docs
+else
+  mdbook build docs
+fi
+
+git checkout -- docs/src/SUMMARY.md
